0b579098999e272c378310b7582f3c981ce6f2ab
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-08-27 23:37:33 -0500

    some changes

--- src/com/instructor/Course.java
+++ src/com/instructor/Course.java
@@ -19,6 +19,7 @@ public class Course {
 	@Column(name="title")
 	private String title;
 	
+	//Many courses has/mapped to one instructor
 	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
